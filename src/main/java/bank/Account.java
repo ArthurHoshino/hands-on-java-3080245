f75c7f7a1c71ba54618ef43f5bc052226f8dc074
Arthur_Hoshino <99358471+ArthurHoshino@users.noreply.github.com>
2022-11-23 23:30:26 +0000

    account class

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,39 @@
+package bank;
+
+public class Account {
+  
+  private int id;
+  private String type;
+  private double balace;
+
+  public Account(int id, String type, double balance) {
+    setId(id);
+    setType(type);
+    setBalace(balance);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public double getBalace() {
+    return this.balace;
+  }
+
+  public void setBalace(double balace) {
+    this.balace = balace;
+  }
+
+}
